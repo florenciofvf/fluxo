bce2aa644e75489f1f5528f4e5c40f92b28cdca7
Florencio Vieira Filho <fvfilho@stefanini.com>
2017-04-26 13:34:21 -0300

    Separador

--- fluxo/src/br/com/florencio/fluxo/Painel.java
+++ fluxo/src/br/com/florencio/fluxo/Painel.java
@@ -102,6 +102,7 @@ public class Painel extends JPanel {
 
 		popupPainel.add(menuItemMinimizarTodos);
 		popupPainel.add(menuItemMaximizarTodos);
+		popupPainel.addSeparator();
 		popupPainel.add(menuItemGerarImagem);
 	}
 
